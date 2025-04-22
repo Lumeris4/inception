c1f852a335c6a8579db9c6427a02e269aac217d7
Lumeris4 <l20langlois@gmail.com>
2025-04-22 15:03:05 +0200

    fin

--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -4,33 +4,32 @@ cd /var/www/html
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-    ./wp-cli.phar core download --allow-root
+./wp-cli.phar core download --allow-root
+echo "Creating config..."
+./wp-cli.phar config create \
+  --dbname=wordpress \
+  --dbuser=wpuser \
+  --dbpass=password \
+  --dbhost=mariadb \
+  --allow-root --debug
 
-    echo "Creating config..."
-    ./wp-cli.phar config create \
-        --dbname=${WORDPRESS_DB_NAME} \
-        --dbuser=${WORDPRESS_DB_USER} \
-        --dbpass=${WORDPRESS_DB_PASSWORD} \
-        --dbhost=${WORDPRESS_DB_HOST} \
-        --allow-root
+echo "Installing WordPress..."
+./wp-cli.phar core install \
+--url=localhost \
+--title=inception \
+--admin_user=${WP_ADMIN_USER} \
+--admin_password=${WP_ADMIN_PASSWORD} \
+--admin_email=${WP_ADMIN_EMAIL} \
+--allow-root
 
-    echo "Installing WordPress..."
-    ./wp-cli.phar core install \
-        --url=${WORDPRESS_URL} \
-        --title="${WORDPRESS_TITLE}" \
-        --admin_user=${WP_ADMIN_USER} \
-        --admin_password=${WP_ADMIN_PASSWORD} \
-        --admin_email=${WP_ADMIN_EMAIL} \
-        --allow-root
-
-    ./wp-cli.phar user create \
-        ${WP_USER} ${WP_USER_EMAIL} \
-        --role=subscriber \
-        --user_pass=${WP_USER_PASSWORD} \
-        --allow-root
+./wp-cli.phar user create \
+${WP_USER} ${WP_USER_EMAIL} \
+--role=subscriber \
+--user_pass=${WP_USER_PASSWORD} \
+--allow-root
 
 echo "Starting php-fpm..."
-php-fpm8.2 -F
+exec php-fpm8.2 -F
 
 
 
