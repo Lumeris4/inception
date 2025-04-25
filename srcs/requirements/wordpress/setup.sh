a74077103780ebd7369663f6382ce90d5866dba6
Lumeris4 <l20langlois@gmail.com>
2025-04-25 12:00:55 +0200

    reglage de version

--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -15,21 +15,21 @@ echo "Creating config..."
 
 echo "Installing WordPress..."
 ./wp-cli.phar core install \
---url=https://lelanglo.42.fr \
---title=inception \
---admin_user=${WP_ADMIN_USER} \
---admin_password=${WP_ADMIN_PASSWORD} \
---admin_email=${WP_ADMIN_EMAIL} \
---allow-root
+ --url=https://lelanglo.42.fr \
+ --title=inception \
+ --admin_user=${WP_ADMIN_USER} \
+ --admin_password=${WP_ADMIN_PASSWORD} \
+ --admin_email=${WP_ADMIN_EMAIL} \
+ --allow-root
 
 ./wp-cli.phar user create \
 ${WP_USER} ${WP_USER_EMAIL} \
---role=subscriber \
---user_pass=${WP_USER_PASSWORD} \
---allow-root
+ --role=subscriber \
+ --user_pass=${WP_USER_PASSWORD} \
+ --allow-root
 
 echo "Starting php-fpm..."
-exec php-fpm8.2 -F
+exec php-fpm7.4 -F
 
 
 
